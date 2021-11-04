3d07de2574501232219f66582351c884104f0c53
Rolkana <rolkaa17@uia.no>
2021-11-04 20:13:21 +0100

    added class MyImage, function get_image and .stl conversion

--- App.py
+++ App.py
@@ -33,11 +33,13 @@ def import_lat():
 
     return lat
 
+
 def import_lon():
     print('Please input desired longitude within Norway')
     lon = float(input())
     if lon > 2.0 and lon < 32.88:
 
+    if lon > 40.18 and lon < 84.17:
         lon = lon
     else:
         print('Longitude out of bounds. Value needs to be between 2 and 32.8')
@@ -77,10 +79,105 @@ request_url = 'https://wms.geonorge.no/skwms1/wms.hoyde-dom?' \
 response = requests.get(request_url, verify=True)  # SSL Cert verification explicitly enabled. (This is also default.)
 print(f"HTTP response status code = {response.status_code}")
 
-img = Image.open(BytesIO(response.content))
-np_img = np.asarray(img)
-# Could do something with numpy here.
-img = Image.fromarray(np.uint8(np_img))
-img.show()
+    img = Image.open(BytesIO(response.content))
+    np_img = np.asarray(img)
+    # Could do something with numpy here.
+    img = Image.fromarray(np.uint8(np_img))
+    img.show()
+    return  img
 
-# Could convert to STL here.
+# Crete the image class based x = lat , y = lon, get_image funtion that gets image based on x and y
+class MyImage:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.img = get_image(x,y)
+
+
+first_image = MyImage(import_lat(), import_lon())
+
+
+
+
+import numpy as np
+import numpy as np
+from stl import mesh
+# Define the 4 vertices of the surface
+
+from PIL import Image
+import matplotlib.pyplot as plt
+img = first_image.img
+grey_img = img.convert('L')
+grey_img.show()
+print(grey_img.size)
+
+max_size=(500,500)
+max_height=10
+min_height=0
+#height=0 for minpix
+#height=maxheight for maxpix
+
+#resize
+grey_img.thumbnail(max_size)
+imageNP= np.array(grey_img)
+maxPix = imageNP.max()
+minPix = imageNP.min()
+
+
+print(imageNP)
+(ncols,nrows)=grey_img.size
+
+verticies=np.zeros((nrows,ncols,3))
+
+for x in range(0,ncols):
+    for y in range(0,nrows):
+        pixelIntensity= imageNP[y][x]
+        z = (pixelIntensity * max_height) / maxPix
+        #coordinates
+        verticies[y][x]=(x,y,z)
+faces=[]
+
+for x in range(0, ncols-1):
+  for y in range(0, nrows-1):
+
+       #create face1
+    vertice1=verticies[y][x]
+    vertice2=verticies[y+1][x]
+    vertice3=verticies[y+1][x+1]
+    face1=np.array([vertice1,vertice2,vertice3])
+       #create face 2
+    vertice1 = verticies[y][x]
+    vertice2 = verticies[y][x+1]
+    vertice3 = verticies[y+1][x+1]
+
+    face2 = np.array([vertice1, vertice2, vertice3])
+
+    faces.append(face1)
+    faces.append(face2)
+print(f"numberof faces:{len(faces)}")
+
+facesNP= np.array(faces)
+# Create the mesh
+surface = mesh.Mesh(np.zeros(facesNP.shape[0], dtype=mesh.Mesh.dtype))
+for i, f in enumerate(faces):
+    for j in range(3):
+        surface.vectors[i][j] = facesNP[i][j]
+# Write the mesh to file "cube.stl"
+surface.save('surface.stl')
+print(surface)
+
+
+
+
+
+
+
+
+
+# lat = 16.8                                #Example input values for debugging purposes - Remove before final release
+# lon = 68.55                               #Example input values for debugging purposes - Remove before final release
+
+
+
+
+
